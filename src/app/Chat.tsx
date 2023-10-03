8a0e538f4c14290f7bcca3fbb37d6de8d3e5f9af
Adesh-Pandey <adeshpandey836@gmail.com>
2023-10-03 16:18:29 +0545

    initial

---
+++ src/app/Chat.tsx
@@ -0,0 +1,3 @@
+export default function Chat(){
+    return <div className="absolute bottom-0 left-0 w-20 h-20 bg-gray-100">chat</div>
+}
---
+++ src/app/context.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { createContext, useContext, useState } from "react"
+
+export const SimulationContext=createContext<null | {
+    simulationSpec:SimulationSpecificationProps|null,
+    setSimulationSpec:(spec:SimulationSpecificationProps)=>void
+}>(null);
+
+export type SimulationSpecificationProps={
+    earth:{}
+    moon:{}
+    sun:{}
+}
+
+export function ContextProvider({children}:{children:React.ReactNode}){
+
+    const [simulationSpec, setSimulationSpec]= useState<SimulationSpecificationProps|null>({
+        earth:{},
+        moon:{},
+        sun:{}
+    });
+
+    return <SimulationContext.Provider value={{
+        simulationSpec,
+        setSimulationSpec
+    }}>
+    {children}
+    </SimulationContext.Provider>
+}
+
+export function useSimulationSpecs(){
+    const context=useContext(SimulationContext);
+    if(!context){
+        throw new Error("useSimulationSpecs must be used within a ContextProvider");
+    }
+    return context
+}
