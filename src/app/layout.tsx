e9be340877f795731f2b71bad4dc668c5d282b76
Adesh-Pandey <adeshpandey836@gmail.com>
2023-10-03 16:34:52 +0545

    path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+        <link rel="icon" href="./public/icon.png" />
       </head>
       <body className="w-full h-full">
         <ContextProvider>{children}</ContextProvider>
