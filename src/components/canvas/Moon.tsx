7ef7ad55b1c14217dbf1344f9800d742415dfc88
Adesh-Pandey <adeshpandey836@gmail.com>
2023-10-04 14:20:24 +0545

    renamed coords

--- src/components/canvas/Moon.tsx
+++ src/components/canvas/Moon.tsx
@@ -1,6 +1,6 @@
 "use client";
 // import {useGLTF} from "@react-three/drei"
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Mesh } from "three";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
@@ -9,16 +9,32 @@ import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
 const Moon = () => {
   const mesh = useRef<Mesh>(null!);
 
+  const [rotationCoord, setRotationCoord] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+
+  useEffect(() => {
+    requestAnimationFrame(() => {
+      setRotationCoord((prev) => ({
+        x: prev.x + 0.002,
+        y: prev.y,
+        z: prev.z,
+      }));
+    });
+  });
+
   const moon = useLoader(GLTFLoader, "Moon.glb");
   try {
     return (
-      <mesh ref={mesh} position={[0, 0, 700]} scale={[0.1, 0.1, 0.1]}>
-        <primitive
-          object={moon.scene}
-          // test value
-
-          rotation={[30, 30, 30]}
-        />
+      <mesh
+        rotation={[rotationCoord.x, rotationCoord.y, rotationCoord.z]}
+        ref={mesh}
+        position={[0, 0, 700]}
+        scale={[0.1, 0.1, 0.1]}
+      >
+        <primitive object={moon.scene} rotation={[30, 30, 30]} />
         <ambientLight intensity={10} />
       </mesh>
     );
