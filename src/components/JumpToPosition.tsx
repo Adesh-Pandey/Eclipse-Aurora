7dff62b1c3abb220362804a02d7c72d6b3176635
SaurabPoudel <poudelsaurab20@gmail.com>
2023-10-05 12:37:44 +0545

    Jump to component setup , functionality remaining and waiting from framer motion from pr of ranjit

---
+++ src/components/JumpToPosition.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { useState } from 'react';
+
+const JumpToPosition = () => {
+  const [month, setMonth] = useState(0);
+  const [day, setDay] = useState(0);
+  const [monthError, setMonthError] = useState('');
+  const [dayError, setDayError] = useState('');
+
+  const handleMonthChange = (e) => {
+    const newMonth = parseInt(e.target.value, 10);
+    if (newMonth >= 1 && newMonth <= 12) {
+      setMonth(newMonth);
+      setMonthError('');
+    } else {
+      setMonthError('Month should be between 1 and 12');
+    }
+  };
+
+  const handleDayChange = (e) => {
+    const newDay = parseInt(e.target.value, 10);
+    if (newDay >= 1 && newDay <= 30) {
+      setDay(newDay);
+      setDayError('');
+    } else {
+      setDayError('Day should be between 1 and 30');
+    }
+  };
+
+
+  return (
+    <div className='absolute bottom-40 flex-col rounded-lg border-2 border-green-500 px-4 py-3 text-green-500'>
+      <p className='pb-2 text-center text-2xl'>Jump to</p>
+      <div className='flex gap-2'>
+        <label htmlFor='month'>Month:</label>
+        <input type='text' inputMode='numeric' pattern='[0-9]+'
+               className='w-[40px] rounded-lg border-green-500 text-black'
+               onChange={handleMonthChange} />
+        <label htmlFor='month'>Day: </label>
+        <input type='text' inputMode='numeric' pattern='[0-9]+'
+               className='w-[40px] rounded-lg border-green-500 text-black'
+               onChange={handleDayChange} />
+      </div>
+      {monthError && <p className=' text-red-500'>{monthError}</p>}
+      {dayError && <p className=' text-red-500'>{dayError}</p>}
+    </div>
+  );
+};
+
+export default JumpToPosition;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,12 @@
-import Chat from "./Chat";
-import Simulation from "./Simuation";
+import Chat from './Chat';
+import Simulation from './Simuation';
+import JumpToPosition from './JumpToPosition';
+import CanvasContainer from '@/components/canvas/CanvasContainer';
 
 export
 {
-    Chat,
-    Simulation
-}
+  Chat,
+  Simulation,
+  JumpToPosition,
+  CanvasContainer,
+};
