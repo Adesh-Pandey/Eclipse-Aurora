7fab7c90bb7bc7d3c017da20ab2eb4486ae552f6
Adesh-Pandey <adeshpandey836@gmail.com>
2023-10-09 13:53:01 +0545

    size

--- src/components/canvas/Sun.tsx
+++ src/components/canvas/Sun.tsx
@@ -10,7 +10,7 @@ const Sun = () => {
   const sun = useLoader(GLTFLoader, 'Sun.glb');
   try {
     return (
-      <mesh ref={mesh} position={[0, 0, 0]} scale={[0.5, 0.5, 0.5]}>
+      <mesh ref={mesh} position={[0, 0, 0]} scale={[0.7, 0.7, 0.7]}>
         <primitive
           object={sun.scene}
           position={[0, 0, -10]}
